49ba23c6e68039e68e4ab23dea95cac8ebfbcc4a
S.Kuntsevich <umdomby@yandex.ru>
2022-01-21 15:30:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+
+app.use(express.json({ extended: true }))
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+// Обработка ошибок, последний Middleware
+app.use(errorHandler)
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL)
+            .then(() => console.log("Successfully connect to MongoDB."))
+            .catch(err => console.error("Connection error", err));
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+start()
